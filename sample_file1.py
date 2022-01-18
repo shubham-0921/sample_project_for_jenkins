2d40744f6387f81f77c5c83c073f25a2ca3a450b
Shubham Singhal <singhal0921@gmail.com>
2022-01-18 17:30:38 +0530

    Adding sample python file

---
+++ sample_file1.py
@@ -0,0 +1,10 @@
+# This is a sample file to integrate jenkins for a python proejct
+
+
+if __name__ == "__main__":
+    list1 = [1, 333, 45, 22]
+    list1.reverse()
+    print(list1)
+
+    list1.sort()
+    print(list1)
